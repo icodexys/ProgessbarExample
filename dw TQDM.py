c4b2a7a648d9536dae43ea93e7240b8d8773cc84
Jheff Mat <18588201+icodexys@users.noreply.github.com>
2021-02-15 20:56:49 -0500

    Update dw TQDM.py

--- dw TQDM.py
+++ dw TQDM.py
@@ -1,20 +1,17 @@
 #  -*- coding: utf-8 -*-
 from tqdm import tqdm
 import requests
- 
- 
- 
 
 url="https://cdnm.meln.top/mr/Aventura%20-%20Cuando%20Volveras.mp3?session_key=69e46c3bf738ecc46eb521c7547462f2&hash=04042964424a6d1682b5f315d3188253"
 file=requests.get(url,stream=True)
 size=int(file.headers['Content-Length'])
- 
-bar=tqdm(range(size),desc="Downloading...:",ncols=100)
 
+chunk=1024
+
+bar=tqdm(iterable=file.iter_content(chunk_size = chunk),total=size/chunk)
 fw=open("file.mp3","wb")
 
-for bs in file.iter_content(chunk_size=1024):
-    bar.update(len(bs))
+for bs in bar:
     fw.write(bs)
 
 
@@ -24,5 +21,4 @@ fw.close()
 
 
 
-chunk=1024
-bar=tqdm(iterable=file.iter_content(chunk_size = chunk),total=size/chunk)
+
