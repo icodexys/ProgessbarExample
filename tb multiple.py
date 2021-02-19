5e6d1982e2d2e1c470afd1cd4efeb4ff6708db30
Jheff Mat <18588201+icodexys@users.noreply.github.com>
2021-02-18 21:11:51 -0500

    Update tb multiple.py

--- tb multiple.py
+++ tb multiple.py
@@ -19,9 +19,7 @@ def byteme(n):
 def iteracion(stream=None,chunk=None,file_handle=None,remaining=None):
     global OriSize,size,Filename,barsize
     size+=len(chunk)
-    print('\r Downloading: [%s%s]%.2f%% ' % ('█' * int(size*barsize/OriSize), 
-    '░'*(barsize-int(size*barsize/OriSize)), float(size/OriSize*100)),
-    byteme(str(size)),' Video: '+Filename+"    " ,end='')
+    print('\r Downloading: [%s%s]%.2f%% ' % ('█' * int(size*barsize/OriSize), '░'*(barsize-int(size*barsize/OriSize)), float(size/OriSize*100)),byteme(str(size)),' Video: '+Filename+"    " ,end='')
 count_dwld=0
 count_failed=0
 
